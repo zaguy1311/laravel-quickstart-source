c39d0f871142e001e22b9f17ae7ccb01782f6318
kampanat <kampanat.m@tangerine.co.th>
2017-11-04 16:25:21 +0700

    first commit

---
+++ composer-update.ps1
@@ -0,0 +1,9 @@
+﻿$pathcurrent=(Get-Item -Path ".\" -Verbose).FullName
+if ([IO.Directory]::Exists($pathnow+"\web\vendor")) 
+    { 
+        docker run -i --rm -v $pathcurrent\web:/app 3dsinteractive/composer:7.1 update 
+    } 
+    else 
+    { 
+        docker run -i --rm -v $pathcurrent\web:/app 3dsinteractive/composer:7.1 install 
+    } 
